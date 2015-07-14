c887f515bd13f1d73cd015dc3921e102c939b06d
Marc de Niverville <marc_alain@gens-MacBook-Air.local>
2015-07-13 21:54:18 -0400

    revised the environment configuration files to run the app and check for MONGO_LAB

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/mean-demo-dev',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-demo',
+
+	// db: 'mongodb://localhost/mean-demo-dev',
+	
 	app: {
 		title: 'MEAN_demo - Development Environment'
 	},
